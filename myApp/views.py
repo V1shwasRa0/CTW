775e1de6c1af6df04fa70907c2330cf0bc8bea11
Vishwas Rao <118887916+V1shwasRa0@users.noreply.github.com>
2024-02-27 17:50:24 +0530

    commit5

--- myApp/views.py
+++ myApp/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 def home(request):
     return render(request,"sample.html")
 
+def home_view(request):
+    return render(request,"home.html")
+
 def about_us(request):
     return render(request,"aboutus.html")
 
