1f423ff055ade8d05a47199c01d1317a0537a797
Vishwas Rao <118887916+V1shwasRa0@users.noreply.github.com>
2024-03-10 17:52:05 +0530

    Update urls.py

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,11 +2,11 @@ from .views import *
 from django.urls import path
 urlpatterns=[
     path("",home,name="home"),
-    path("about-us/",about_us,name="about_us"),
-    path('our-approach/', our_approach, name='our_approach'),
+    path("about_us",about_us,name="about_us"),
+    path('our_approach', our_approach, name='our_approach'),
     path('our-impact/', our_impact, name='our_impact'),
-    path('partnerships/', c_p, name='c_p'),
-    path('volunteering/', vol, name='vol'),
-    path('blog/', blog, name='blog'),
-    path('contact/', contact_us, name='contact_us'),
+    path('c_p/', c_p, name='c_p'),
+    path('vol', vol, name='vol'),
+    path('blog', blog, name='blog'),
+    path('contact_us', contact_us, name='contact_us'),
 ]
