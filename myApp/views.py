a40aa7a9d61c1d16e4954eabc14078fd91dfaec7
Vishwas Rao <118887916+V1shwasRa0@users.noreply.github.com>
2024-03-10 17:47:41 +0530

    Update views.py

--- myApp/views.py
+++ myApp/views.py
@@ -6,19 +6,19 @@ def home(request):
     return render(request,"home.html")
 
 def about_us(request):
-    return render(request,"aboutUs.html")
+    return render(request,"aboutus.html")
 
 def our_approach(request):
-    return render(request, 'ourApproach.html')
+    return render(request, 'our_approach.html')
 
 def our_impact(request):
     return render(request, 'ourImpact.html')
 
 def c_p(request):
-    return render(request, 'corporate.html')
+    return render(request, 'c_p.html')
 
 def vol(request):
-    return render(request, 'volunteering.html')
+    return render(request, 'vol.html')
 
 def blog(request):
     return render(request, 'blog.html')
